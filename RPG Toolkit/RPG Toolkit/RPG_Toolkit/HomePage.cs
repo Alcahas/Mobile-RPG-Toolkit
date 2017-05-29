6e43f51e3e4af2feee9f2b6082cf058f5ca89077
Alcahas <jurjvlad96@gmail.com>
2017-05-29 20:13:11 +0300

    Steppers actually work now and show their current value. Also, tweaked the naming a bit and added a lot of comments

--- RPG Toolkit/RPG Toolkit/RPG_Toolkit/HomePage.cs
+++ RPG Toolkit/RPG Toolkit/RPG_Toolkit/HomePage.cs
@@ -12,40 +12,48 @@ namespace RPG_Toolkit
     {
         public HomePage()
         {
-            StackLayout scrollstack = new StackLayout();
-            Color a = Color.White;
-        
+            StackLayout scrollstack = new StackLayout(); //scrollstack template
+            Color a = Color.White; //default color
+//--------------------------------------------------------------------------------
+//------------------------------Visual elements------------------------------------
+//-----------------------------------------------------------------------------------        
+
             Button bAdd = new Button //button to add an item 
             {
                 Text = "Add",
              //   HorizontalOptions = LayoutOptions.EndAndExpand //put it on the right
-            };         
+            };    
+            
             Button bRemove = new Button //button to remove an item
             {
                 Text = "Remove",
                // HorizontalOptions = LayoutOptions.EndAndExpand //put it on the right
             };
+
             Picker colorPicker = new Picker //picker to hold multiple countable types (tokens, points, etc)
             {
                 Title = "Select from this list",    
                 //HorizontalOptions = LayoutOptions.StartAndExpand //put it on the left
             };
+
             Entry nameEntry = new Entry //entry field to insert the name of an item
             {
                 Placeholder = "Enter name",
                 Text = "",
                 //HorizontalOptions = LayoutOptions.CenterAndExpand //put it in the center
             };
+
             Picker typePicker = new Picker //picker to hold the type
             { 
               Title = "Choose type",
              // HorizontalOptions = LayoutOptions.StartAndExpand //put it on the left
             };
-            Dictionary<string, Color> nameToColor = new Dictionary<string, Color>
+
+            Dictionary<string, Color> nameToColor = new Dictionary<string, Color> //dictionary to hold all the key-value color pairings in the picker
             {
                 {"White", Color.White},{"Light Gray", Color.LightGray },{"Red", Color.IndianRed}, {"Green", Color.LightGreen}, {"Blue", Color.LightBlue}, {"Yellow", Color.LightYellow}
             };
-            foreach (string colorName in nameToColor.Keys)
+            foreach (string colorName in nameToColor.Keys) //populating the picker with the dictionary items above
             {
                 colorPicker.Items.Add(colorName);
             }
@@ -57,6 +65,8 @@ namespace RPG_Toolkit
             }
 
 //---------------------------------------------------------------------------------
+//------------------------------Containers------------------------------------------
+//-----------------------------------------------------------------------------------
             StackLayout first = new StackLayout //first stack from the top
             {
                 Orientation = StackOrientation.Horizontal, //make it put the items horizontally
@@ -65,24 +75,20 @@ namespace RPG_Toolkit
                     nameEntry, colorPicker              
                 }
             };
+
             StackLayout second = new StackLayout //second stack from the top
             {
-              Orientation = StackOrientation.Horizontal, //make it put the items horizontally
+                Orientation = StackOrientation.Horizontal, //make it put the items horizontally
                 Children={
                     typePicker, bRemove, bAdd
                 }
-            };
-            //StackLayout third = new StackLayout //third stack from the top
-            //{
-            //    Orientation = StackOrientation.Horizontal, //make it put the items horizontally
-            //    Children = {
-            //        typePicker, bRemove
-            //    }
-            //};            
+            };     
+            
             ScrollView scroll = new ScrollView // the scrollview to hold the created items
             {
                 Content = scrollstack
             };
+
             StackLayout scrollcontainer = new StackLayout //stack containing the scroll
             {
                 Children =
@@ -90,6 +96,7 @@ namespace RPG_Toolkit
                     scroll
                 }
             };
+
             StackLayout topstack = new StackLayout // stack containing the upper elements
             {
                 Children =
@@ -97,12 +104,14 @@ namespace RPG_Toolkit
                     first, second, //third
                 }
             };
+
             Frame topframe = new Frame //frame containing the upper elements
             { 
                 Content = topstack,
                 OutlineColor = Color.Silver,
                 BackgroundColor = Color.LightGray
             };         
+
             StackLayout finalstack = new StackLayout
             {
                 Children =
@@ -111,16 +120,19 @@ namespace RPG_Toolkit
 
                 }
             };
-//-------------------------------------------------------------------------------------------
-            colorPicker.SelectedIndexChanged += (sender, args) =>
+
+//---------------------------------------------------------------------------------
+//------------------------------Event handlers--------------------------------------
+//-----------------------------------------------------------------------------------
+            colorPicker.SelectedIndexChanged += (sender, args) => //event to handle the index change (selection) of the color Picker
             {
-                if (colorPicker.SelectedIndex == -1)
+                if (colorPicker.SelectedIndex == -1) //default value
                 {
-                    a = Color.White;
+                    a = Color.White; 
                 }
                 else
                 {
-                    string colorName = colorPicker.Items[colorPicker.SelectedIndex];
+                    string colorName = colorPicker.Items[colorPicker.SelectedIndex]; //setting the value using the dictionary
                     a = nameToColor[colorName];
                 }
             };
@@ -129,14 +141,16 @@ namespace RPG_Toolkit
             bAdd.Clicked += AddClicked; //event for the Add button click
             void AddClicked(object sender, EventArgs args)
             {
-                Label childLabel = new Label
+                string savedName = nameEntry.Text; // saving the data in the Entry box
+                Label childLabel = new Label //simple label also used as default
                 {
-                    Text = nameEntry.Text,
+                    Text = savedName,
                     FontSize = 26,
                     HorizontalOptions = LayoutOptions.StartAndExpand
 
                 };
-                Stepper childStepper = new Stepper
+
+                Stepper childStepper = new Stepper //the stepper constructor, object is made by pressing the Add button after choosing Stepper from the picker
                 {
                     Value = 0,
                     Minimum = 0,
@@ -145,8 +159,19 @@ namespace RPG_Toolkit
                     HorizontalOptions = LayoutOptions.EndAndExpand
                 };
 
+                Label stepperLabel = new Label //label to be used with its corresponding stepper
+                {
+                    Text = String.Format("{0}: {1}", savedName, childStepper.Value), //setting the value of the label to the correct format and current stepper value
+                    FontSize = 26,
+                    HorizontalOptions = LayoutOptions.StartAndExpand
+                };
 
-                if (typePicker.SelectedIndex == -1)
+                childStepper.ValueChanged += OnStepperValueChanged;
+                void OnStepperValueChanged (object Sender, ValueChangedEventArgs e)
+                {
+                    stepperLabel.Text = String.Format("{0}: {1}", savedName,childStepper.Value);
+                }
+                if ((typePicker.SelectedIndex == -1) || (typePicker.SelectedIndex == 0)) //adding the simple label for the default picker value and label option
                 {
                     scrollstack.Children.Add(new Frame
                     {
@@ -154,24 +179,15 @@ namespace RPG_Toolkit
                         BackgroundColor = a,
                         OutlineColor = Color.Silver
                     });
-                }
-                if (typePicker.SelectedIndex == 0)
-                {
-                    scrollstack.Children.Add(new Frame
-                    {
-                        Content = childLabel,
-                        BackgroundColor = a,
-                        OutlineColor = Color.Silver
-                    });
-                }
+                }                               
                 if (typePicker.SelectedIndex == 1)
                 {
                     StackLayout stepperLayout = new StackLayout()
                     {
-                        Orientation = StackOrientation.Horizontal,
+                        //Orientation = StackOrientation.Horizontal,
                         Children =
                         {
-                            childLabel, childStepper
+                            stepperLabel, childStepper
                         }
                     };
 
@@ -193,7 +209,11 @@ namespace RPG_Toolkit
                 }
             }
 
-
+            
+            
+//---------------------------------------------------------------------------------
+//------------------------------Populating the final stack--------------------------
+//-----------------------------------------------------------------------------------
             Content = finalstack;   
         }        
     }
